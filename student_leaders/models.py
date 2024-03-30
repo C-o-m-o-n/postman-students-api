94381c65ff8ace1a38034df3acc82012ef19046f
c-o-m-o-n <comon928@gmail.com>
2024-03-30 15:57:25 +0300

    rm social links list

--- student_leaders/models.py
+++ student_leaders/models.py
@@ -4,22 +4,38 @@ class Student(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     area_of_interest = models.CharField(max_length=255)
+    
+    def __str__(self):
+      return self.name
 
 class Speaker(models.Model):
     name = models.CharField(max_length=255)
     occupation = models.CharField(max_length=255)
     picture_url = models.URLField(blank=True)
-    social_links = models.JSONField(default=list)
+    # social_links = models.JSONField()
+    github_link = models.URLField(blank=True)
+    x_link = models.URLField(blank=True)
+    linkedin_link = models.URLField(blank=True)
+      
+    
+    def __str__(self):
+      return self.name
 
 class Event(models.Model):
     date = models.DateField()
     time = models.TimeField()
     topic = models.CharField(max_length=255)
     speaker = models.ForeignKey(Speaker, on_delete=models.SET_NULL, blank=True, null=True)
+    
+    def __str__(self):
+      return self.topic
 
 class Registration(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    
+    def __str__(self):
+      return self.student
 
 
 
--- student_leaders/serializers.py
+++ student_leaders/serializers.py
@@ -17,13 +17,13 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class SpeakerSerializer(serializers.ModelSerializer):
-    social_links = serializers.ListField(child=serializers.DictField())
+    # social_links = serializers.ListField(child=serializers.DictField())
 
     class Meta:
         model = Speaker
         fields = '__all__'
 
     def create(self, validated_data):
-        social_links_data = validated_data.pop('social_links', [])  # Get social links data or empty list if not provided
+        
         speaker = Speaker.objects.create(**validated_data)
         return speaker
