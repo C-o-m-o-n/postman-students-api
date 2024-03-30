4e51728872886e6327cb81d3e5233f6e80750abe
c-o-m-o-n <comon928@gmail.com>
2024-03-30 16:30:14 +0300

    add event reports

--- student_leaders/models.py
+++ student_leaders/models.py
@@ -26,9 +26,33 @@ class Event(models.Model):
     time = models.TimeField()
     topic = models.CharField(max_length=255)
     speaker = models.ForeignKey(Speaker, on_delete=models.SET_NULL, blank=True, null=True)
+    EVENT_TYPE_CHOICES = (
+        ('virtual', 'virtual'),
+        ('in-person', 'in-person'),
+        ('hybrid', 'hybrid'),)
+    event_type = models.CharField(max_length=200, choices=EVENT_TYPE_CHOICES, default='virtual')
     
     def __str__(self):
       return self.topic
+      
+class Event_report(models.Model):
+  event_date = models.DateField()
+  event_time = models.TimeField()
+  speaker = models.ForeignKey(Speaker, on_delete=models.SET_NULL, blank=True, null=True)
+  registered_students = models.IntegerField()
+  attendees = models.IntegerField()
+  college_name = models.CharField(max_length=255)
+  collaboring_college = models.CharField(max_length=255)
+  EVENT_TYPE_CHOICES = (
+        ('virtual', 'virtual'),
+        ('in-person', 'in-person'),
+        ('hybrid', 'hybrid'), )
+  event_type = models.CharField(max_length=200, choices=EVENT_TYPE_CHOICES, default='virtual')
+  platform = models.CharField(max_length=255)
+  media_link = models.URLField(blank=True)
+    
+  def __str__(self):
+    return self.topic
 
 class Registration(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
